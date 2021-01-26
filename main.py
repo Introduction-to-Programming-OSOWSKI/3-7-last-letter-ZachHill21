0fc00c29905140df6a8b0305341165e2eb3a8fcc
ZachHill21 <814055@mystma.org>
2021-01-26 18:48:08 +0000

    try 5

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
-def lastLetter(a): 
-    a = a + " "
-    for i in range(len(a)): 
-        if a[i] == ' ': 
-            print(a[i - 1], end = " ") 
+def lastLetter(w): 
+    w = w + " "
+    for i in range(len(w)): 
+        if w[i] == ' ': 
+            print(w[i - 1], end = " ") 
   
-a = "a"
-lastLetter(a) 
+
+lastLetter("minnesota") 
